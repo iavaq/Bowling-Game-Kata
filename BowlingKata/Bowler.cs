0ba4f0766b1cb7feb039dbd8c2a4f227075966c2
Ia <iavaquilar@gmail.com>
2022-05-22 20:50:46 +0100

    project setup

---
+++ BowlingKata/Bowler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingKata
+{
+    public class Bowler
+    { 
+        public string FrameScores { get; private set; }
+        public int TotalScore { get; private set; }
+
+        public Bowler(string frameScores)
+        {
+            FrameScores = frameScores;
+            TotalScore = GetTotalScore();
+        }
+
+        public int GetTotalScore()
+        {
+            int runningTotal = 0;
+            char[] delimiters = { ' ', ',', '.', ';', '\t' };
+            string[] scores = FrameScores.Split(delimiters);
+
+            for (int i=0; i<=scores.Length; i++)
+            {
+                if (scores[i].Equals("x"))
+                {
+                    runningTotal += 10 + Int32.Parse(scores[i + 1]) + Int32.Parse(scores[i + 2]);
+                }
+            }
+
+            return runningTotal;
+        }
+    }
+}
---
+++ BowlingKataTests/BowlerTests.cs
@@ -0,0 +1,16 @@
+namespace BowlingKataTests
+{
+    public class BowlerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
